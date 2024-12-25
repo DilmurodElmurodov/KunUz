9b27686a90352194c66be7467888f7c89f64529f
Dilmurod <dilmurodelmurodov590@gmail.com>
2024-12-26 04:26:36 +0500

    add project

---
+++ src/main/java/org/example/kunuzproject/KunUzprojectApplication.java
@@ -0,0 +1,13 @@
+package org.example.kunuzproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunUzprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KunUzprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kunuzproject/KunUzprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kunuzproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunUzprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
